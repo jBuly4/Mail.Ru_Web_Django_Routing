595c0dece1d007984a69f52fd8d5896ee6c450f6
jBuly4 <ibuly4@gmail.com>
2022-03-28 21:00:36 +0300

    added solution to main.py

--- main.py
+++ main.py
@@ -1,16 +1,66 @@
-# This is a sample Python script.
+""" solution """
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# urls.py
+
+from django.conf.urls import url
+
+from routing.views import (
+    simple_route, slug_route, sum_route,
+    sum_get_method, sum_post_method,
+)
+
+urlpatterns = [
+    url(r'^simple_route/$', simple_route),
+    url(r'^slug_route/([a-z0-9-_]{1,16})/$', slug_route),
+    url(r'^sum_route/(-?\d+)/(-?\d+)/$', sum_route),
+    url(r'^sum_get_method/$', sum_get_method),
+    url(r'^sum_post_method/$', sum_post_method),
+]
+
+# views.py
+
+from django.http import HttpResponse
+from django.views.decorators.http import require_GET, require_POST
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+@require_GET
+def simple_route(request):
+    return HttpResponse()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def slug_route(request, slug):
+    return HttpResponse(slug)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def sum_route(request, a, b):
+    try:
+        a = int(a)
+        b = int(b)
+    except (ValueError, TypeError):
+        return HttpResponse(status=400)
+
+    return HttpResponse(a + b)
+
+
+@require_GET
+def sum_get_method(request):
+    try:
+        a = int(request.GET.get('a'))
+        b = int(request.GET.get('b'))
+    except (ValueError, TypeError):
+        return HttpResponse(status=400)
+
+    return HttpResponse(a + b)
+
+
+@require_POST
+def sum_post_method(request):
+    try:
+        a = int(request.POST.get('a'))
+        b = int(request.POST.get('b'))
+    except (ValueError, TypeError):
+        return HttpResponse(status=400)
+
+    return HttpResponse(a + b)
+
+
